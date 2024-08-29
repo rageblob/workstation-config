e8ce8755a2dc1dd25cccac47302963bd8cc09b23
rageblob <39024407+rageblob@users.noreply.github.com>
2024-08-29 02:18:18 +0200

    chore(file structure): rename some files

--- scripts/zsh_config.sh
+++ scripts/zsh_config.sh
@@ -3,13 +3,13 @@
 # create zsh config folder if it doesn't exist
 # otherwise create a backup
 if ! [[ -d "$HOME/.config/zsh" ]]; then
-	mkdir $HOME/.config/zsh/
+  mkdir $HOME/.config/zsh/
 else
-	mv $HOME/.config/zsh{,.bak}
+  mv $HOME/.config/zsh{,.bak}
 fi
 
 # link the config files to the zsh config folder
-ln $PWD/src/zsh/aliases $HOME/.config/zsh/aliases
-ln $PWD/src/zsh/env $HOME/.config/zsh/env
-ln $PWD/src/zsh/settings $HOME/.config/zsh/settings
-ln $PWD/src/zsh/xdg_spec $HOME/.config/zsh/xdg_spec
+ln $PWD/src/shell/aliases $HOME/.config/zsh/aliases
+ln $PWD/src/shell/env $HOME/.config/zsh/env
+ln $PWD/src/shell/zsh_cfg $HOME/.config/zsh/settings
+ln $PWD/src/shell/xdg_spec $HOME/.config/zsh/xdg_spec
