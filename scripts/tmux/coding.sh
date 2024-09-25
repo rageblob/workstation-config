035346bf65a089aef8b0f0cec5720d532eb74972
rageblob <39024407+rageblob@users.noreply.github.com>
2024-09-25 20:38:51 +0200

    chore(scripts): streamline custom tmux session script
    And rename variable.

--- scripts/tmux/coding.sh
+++ scripts/tmux/coding.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-SESH="coding"
+SESSION="coding"
 # check for session
-tmux has-session -t $SESH 2>/dev/null
+tmux has-session -t $SESSION 2>/dev/null
 # define desired session structure
 if [[ $? != 0 ]]; then
   # create new session and rename first window
-  tmux new-session -d -s $SESH -n "editor"
+  tmux new-session -d -s $SESSION -n "editor"
   # cd into coding dir and open nvim
-  tmux send-keys -t $SESH:editor "cd ~/Documents/Code/" C-m
-  tmux send-keys -t $SESH:editor "nvim ." C-m
+  tmux send-keys -t $SESSION:editor "cd ~/Documents/Code/" C-m
+  tmux send-keys -t $SESSION:editor "nvim ." C-m
   # create second window
-  tmux new-window -t $SESH -n "home"
-  tmux send-keys -t $SESH:home "cd ~" C-m
+  tmux new-window -t $SESSION -n "home"
+  tmux send-keys -t $SESSION:home "cd ~" C-m
+  tmux send-keys -t $SESSION:home "clear" C-m
   # select first window
-  tmux select-window -t $SESH:editor
+  tmux select-window -t $SESSION:editor
 fi
 # attach the session
-tmux a -t $SESH
+tmux a -t $SESSION
